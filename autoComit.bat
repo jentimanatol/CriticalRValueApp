3855562987d6b09d0bb18b6fbd076f18a7a9bb29
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2025-05-02 23:06:41 -0400

    'Final commit before release'

--- autoComit.bat
+++ autoComit.bat
@@ -4,6 +4,6 @@ git commit -m "'Final commit before release'"
 git push origin main
 
 :: === Tagging for GitHub Actions Release Build ===
-git tag v3.0
-git push origin v3.0
+git tag v3.1
+git push origin v3.1
 pause
