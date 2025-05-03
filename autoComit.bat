9ade795d7fb5ff3991ac3a2134352100641f1e6a
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2025-05-02 20:27:08 -0400

    Add exit button to the main form

--- autoComit.bat
+++ autoComit.bat
@@ -4,6 +4,6 @@ git commit -m "Add exit button to the main form"
 git push origin main
 
 :: === Tagging for GitHub Actions Release Build ===
-git tag v2.6.2
-git push origin v2.6.2
+git tag v2.6.3
+git push origin v2.6.3
 pause
