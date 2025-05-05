57e60b85e5aec7eee44943f0364a0773f03cc54a
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2025-05-05 02:02:16 -0400

    'Window size'

--- autoComit.bat
+++ autoComit.bat
@@ -4,6 +4,6 @@ git commit -m "'Window size'"
 git push origin main
 
 :: === Tagging for GitHub Actions Release Build ===
-git tag v3.5
-git push origin v3.5
+git tag v4.0
+git push origin v4.0
 pause
